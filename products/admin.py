a367b08a4e10218aec05a21037105320018ba209
harrison townend <harrisontownend@hotmail.com>
2026-01-27 01:11:34 +0000

    Registered SweetCategory model into admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import SweetCategory
+
 # Register your models here.
+
+
+@admin.register(SweetCategory)
+class SweetCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    search_field = ('name',)
