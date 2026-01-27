6f7a5cbc023ea9258431f5bdd7e9a0a2eaac8013
harrison townend <harrisontownend@hotmail.com>
2026-01-27 14:10:55 +0000

    Added search fields for admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.utils.html import mark_safe
 from .models import SweetCategory, DietaryTag, Product
 
 # Register your models here.
@@ -8,22 +7,28 @@ from .models import SweetCategory, DietaryTag, Product
 @admin.register(SweetCategory)
 class SweetCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'id')
-    search_field = ('name',)
+    search_fields = ('name',)
 
 
 @admin.register(DietaryTag)
 class DietaryTagAdmin(admin.ModelAdmin):
     list_display = ('name', 'id')
-    search_field = ('name',)
+    search_fields = ('name',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'name',
+        'id',
         'sku',
         'price',
         'sweet_category',
         'image',
         'in_stock',
+    )
+
+    search_fields = (
+        'name',
+        'sku',
     )
