73a1cdb1db29a87af58433469259cfca3f48c5ee
harrison townend <harrisontownend@hotmail.com>
2026-01-27 12:28:28 +0000

    Added dietarytag model and dumpdata into json files

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import SweetCategory
+from .models import SweetCategory, DietaryTag
 
 # Register your models here.
 
 
 @admin.register(SweetCategory)
 class SweetCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    search_field = ('name',)
+
+@admin.register(DietaryTag)
+class DietaryTagAdmin(admin.ModelAdmin):
     list_display = ('name', 'id')
     search_field = ('name',)
--- products/models.py
+++ products/models.py
@@ -5,7 +5,15 @@ from django.db import models
 
 class SweetCategory(models.Model):
 
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DietaryTag(models.Model):
+
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
