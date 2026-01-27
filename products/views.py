a91c82476842ecfeae056ec8374b67648ad57890
harrison townend <harrisontownend@hotmail.com>
2026-01-27 19:52:47 +0000

    Added simple name or description search with q import

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models import Q
 from .models import SweetCategory, DietaryTag, Product
 
 # Create your views here.
@@ -8,8 +8,19 @@ from .models import SweetCategory, DietaryTag, Product
 def all_products(request):
     products = Product.objects.all()
 
+    query = None
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if query:
+            products = products.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+
     context = {
         'products': products,
+        'search': query,
     }
 
     return render(request, 'products/product-list.html', context)
