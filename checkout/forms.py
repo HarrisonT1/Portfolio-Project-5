375d26861af32aaa327ac3a51a5ca118ffca1ceb
harrison townend <harrisontownend@hotmail.com>
2026-01-29 22:23:47 +0000

    Added form for order model

---
+++ checkout/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            'full_name',
+            'email',
+            'phone_number',
+            'country',
+            'postcode',
+            'city',
+            'town',
+            'street_address1',
+            'street_address2',
+            'delivery_method',
+        ]
