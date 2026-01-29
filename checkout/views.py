bd53c1d8e90dac4f2d6a93cfc6abdda2d8743f13
harrison townend <harrisontownend@hotmail.com>
2026-01-29 22:44:51 +0000

    Added view for checkout logic

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import OrderForm
+from .models import OrderLineItem
+from products.models import Product
 
 # Create your views here.
 
 
 def checkout(request):
+    bag = request.session.get('bag', {})
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.save()
 
-    return render(request, 'checkout/checkout.html')
+            for item_slug, quantity in bag.items():
+                product = get_object_or_404(Product, slug=item_slug)
+                OrderLineItem.objects.create(
+                    order=order,
+                    product=product,
+                    quantity=quantity,
+                    line_item_total=product.price * quantity
+                )
+
+            request.session['bag'] = {}
+            return redirect('checkout_success', order_number=order.order_number)
+    else:
+        form = OrderForm()
+
+    bag_items = []
+    subtotal = 0
+
+    for item_slug, quantity in bag.items():
+        product = get_object_or_404(Product, slug=item_slug)
+        total = product.price * quantity
+        subtotal += total
+        bag_items.append({
+            'product': product,
+            'quantity': quantity,
+            'total': total
+        })
+
+    context = {
+        'form': form,
+        'quantity': bag_items,
+        'subtotal': total,
+    }
+    return render(request, 'checkout/checkout.html', context)
