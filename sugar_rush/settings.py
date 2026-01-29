9275b65c548a44a1af12368fc085551cda095d2f
harrison townend <harrisontownend@hotmail.com>
2026-01-29 13:18:28 +0000

    Created checkout app and connected to project

--- sugar_rush/settings.py
+++ sugar_rush/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     # Local apps
     'products',
     'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -158,3 +159,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 30
+STANDARD_DELIVERY_PERCENTAGE = 10
