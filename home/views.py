14a35d046e9ac5927365847d2c046fedbb7ea09a
harrison townend <harrisontownend@hotmail.com>
2026-01-31 20:57:19 +0000

    Allowed reviews to render on the home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from reviews.models import Review
 
 # Create your views here.
 
 
 def home(request):
+    reviews = Review.objects.filter(approved=True)
 
-    return render(request, 'home/home.html')
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, 'home/home.html', context)
