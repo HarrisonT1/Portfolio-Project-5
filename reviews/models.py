59775f5175e1c07ff1d4013d4458049c0fa860fa
harrison townend <harrisontownend@hotmail.com>
2026-01-31 20:13:48 +0000

    Added reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Review(models.Model):
+    # remove null and blank in future
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='reviews'
+    )
+    rating = models.PositiveSmallIntegerField()
+
+    name = models.TextField(max_length=100)
+    review_message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
