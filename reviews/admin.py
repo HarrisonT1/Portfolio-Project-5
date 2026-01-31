fbe176db2e9de85e7cc39f370a9aa220d2563e71
harrison townend <harrisontownend@hotmail.com>
2026-01-31 20:20:59 +0000

    Added reviews to admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Review
 
 # Register your models here.
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'rating',
+        'approved',
+        'created_at',
+    )
+
+    list_filter = (
+        'created_at',
+        'name',
+    )
+
+    ordering = ('-created_at',)
