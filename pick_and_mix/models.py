8c5eb3041ff5e4da7f2a81b45a7c5da28e6de5d2
harrison townend <harrisontownend@hotmail.com>
2026-02-01 21:57:51 +0000

    Created models and made migrations

--- pick_and_mix/models.py
+++ pick_and_mix/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.text import slugify
+
+from products.models import Product
 
 # Create your models here.
+
+
+class PickAndMixBag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    max_weight_in_grams = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    sweets = models.ManyToManyField(Product, blank=True, related_name='pick_and_mix_bag')
+
+    def save(self, *args, **kwargs):
+        """
+        A function to automatically create a slug name
+        for cleaner urls and database sorting
+        """
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
