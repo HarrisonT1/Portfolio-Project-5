07eb6e111f3088a7463d04d2d65c80e44a09c025
harrison townend <harrisontownend@hotmail.com>
2026-02-01 22:25:55 +0000

    Render each bag on pick and mix page for user selection

--- pick_and_mix/admin.py
+++ pick_and_mix/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PickAndMixBag
 
 # Register your models here.
+
+
+@admin.register(PickAndMixBag)
+class PickAndMixBagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'max_weight_in_grams')
+    filter_horizontal = ('sweets',)
--- pick_and_mix/views.py
+++ pick_and_mix/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import PickAndMixBag
 
 # Create your views here.
 
 
 def pick_and_mix(request):
-    return render(request, 'pick_and_mix/pick-and-mix.html')
+    pnmbag = PickAndMixBag.objects.all()
+
+    context = {
+        'pnmbag': pnmbag,
+    }
+
+    return render(request, 'pick_and_mix/pick-and-mix.html', context)
