cb685d6dd7106a33f062362675456a9fb2e41443
harrison townend <harrisontownend@hotmail.com>
2026-02-03 18:12:49 +0000

    Fix uncaught reference error in stripe checkout

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-    var client_secret = $('#id_client_secret').text().slice(1, -1);
+    var clientSecret = $('#id_client_secret').text().slice(1, -1);
 
     var stripe = Stripe(stripe_public_key);
     var elements = stripe.elements();
