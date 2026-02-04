4405a86fafe08c5d4e03561c142b05b7ef4600d9
harrison townend <harrisontownend@hotmail.com>
2026-02-04 14:48:21 +0000

    Reformatted checkouts.py to better work with stripe

--- checkout/views.py
+++ checkout/views.py
@@ -8,19 +8,21 @@ from .utils import calc_delivery_cost, create_line_items, create_order, stripe_p
 
 
 def checkout(request):
-    bag = request.session.get('bag', {})
-
-    if not bag:
-        return redirect('products')
 
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+    delivery_method = request.POST.get('delivery_method', 'standard')
+
     if request.method == 'POST':
+        bag = request.session.get('bag', {})
+
+        if not bag:
+            return redirect('products')
+
         form = OrderForm(request.POST)
         if form.is_valid():
             order = create_order(form)
-            delivery_method = request.POST.get('delivery_method', 'standard')
             order.delivery_method = delivery_method
             order.save(update_fields=['delivery_method'])
 
@@ -28,34 +30,38 @@ def checkout(request):
 
             order.update_total()
 
-            intent = stripe_payment_intent(order.grand_total, stripe_secret_key)
-
             request.session['bag'] = {}
 
             return redirect('checkout_success', order_number=order.order_number)
     else:
         form = OrderForm()
 
-    bag_items, order_total = create_line_items(bag)
+        bag = request.session.get('bag', {})
 
-    delivery_cost = calc_delivery_cost(order_total, delivery_method)
-    grand_total = order_total + delivery_cost
+        if not bag:
+            return redirect('products')
 
-    context = {
-        'form': form,
-        'bag_items': bag_items,
-        'order_total': order_total,
-        'delivery_cost': delivery_cost,
-        'grand_total': grand_total,
-        'FREE_DELIVERY_THRESHOLD': settings.FREE_DELIVERY_THRESHOLD,
-        'STANDARD_DELIVERY_COST': settings.STANDARD_DELIVERY_COST,
-        'PREMIUM_DELIVERY_COST': settings.PREMIUM_DELIVERY_COST,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-        'delivery_method': delivery_method
-    }
+        bag_items, order_total = create_line_items(bag)
 
-    return render(request, 'checkout/checkout.html', context)
+        delivery_cost = calc_delivery_cost(order_total, delivery_method)
+        grand_total = order_total + delivery_cost
+        intent = stripe_payment_intent(grand_total, stripe_secret_key)
+
+        context = {
+            'form': form,
+            'bag_items': bag_items,
+            'order_total': order_total,
+            'delivery_cost': delivery_cost,
+            'grand_total': grand_total,
+            'FREE_DELIVERY_THRESHOLD': settings.FREE_DELIVERY_THRESHOLD,
+            'STANDARD_DELIVERY_COST': settings.STANDARD_DELIVERY_COST,
+            'PREMIUM_DELIVERY_COST': settings.PREMIUM_DELIVERY_COST,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+            'delivery_method': delivery_method,
+        }
+
+        return render(request, 'checkout/checkout.html', context)
 
 
 def checkout_success(request, order_number):
