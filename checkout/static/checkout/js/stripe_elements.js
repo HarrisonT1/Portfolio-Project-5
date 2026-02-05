ccb887422e91f3e2f92b669cbb019a66de0f30e1
harrison townend <harrisontownend@hotmail.com>
2026-02-05 01:22:16 +0000

    Added metadata and form details to the payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -48,7 +48,31 @@ $(document).ready(function() {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address: {
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.city.value),
+                        town: $.trim(form.town.value),
+                        country: $.trim(form.country.value),
+                    }
+                }
+            },
+        shipping: {
+            name: $.trim(form.full_name.value),
+            phone: $.trim(form.phone_number.value),
+            address: {
+                line1: $.trim(form.street_address1.value),
+                line2: $.trim(form.street_address2.value),
+                city: $.trim(form.city.value),
+                town: $.trim(form.town.value),
+                postcode: $.trim(form.postcode.value),
+                country: $.trim(form.country.value),
             }
+        }
         }).then(function(result) {
             if (result.error) {
                 var errorDiv = document.getElementById('card-errors');
