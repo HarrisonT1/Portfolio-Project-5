3174f8004edee12e26912df1362a9c6343ceef19
harrison townend <harrisontownend@hotmail.com>
2026-02-06 15:12:52 +0000

    Created view and template to render accounts form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserAccountForm
 # Create your views here.
 
 
+@login_required
 def view_account(request):
-    return render(request, 'accounts/account.html')
+    account = request.user
+
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST, instance=account)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+    else:
+        form = UserAccountForm(instance=account)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/account.html', context)
