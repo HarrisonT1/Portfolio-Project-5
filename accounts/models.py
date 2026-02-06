d708485a8d8f0cc7039095d67ebe04ab122a9581
harrison townend <harrisontownend@hotmail.com>
2026-02-06 14:54:01 +0000

    Created UserAccount model and migrate to database

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    default_full_name = models.CharField(max_length=60, blank=True)
+    default_email = models.CharField(max_length=60, blank=True)
+    default_phone_number = models.CharField(max_length=60, blank=True)
+    default_country = models.CharField(max_length=60, blank=True)
+    default_postcode = models.CharField(max_length=60, blank=True)
+    default_city = models.CharField(max_length=60, blank=True)
+    default_town = models.CharField(max_length=60, blank=True)
+    default_street_address1 = models.CharField(max_length=60, blank=True)
+    default_street_address2 = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        return self.user.username
