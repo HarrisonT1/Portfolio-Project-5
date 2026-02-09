01fc2bdb6637dc5c27c321e6026b3a8706522593
harrison townend <harrisontownend@hotmail.com>
2026-02-09 03:06:31 +0000

    Finished styling allauth templates, added email_host_user and email_host_password

--- sugar_rush/settings.py
+++ sugar_rush/settings.py
@@ -205,8 +205,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-# EMAIL_HOST_USER = 'email from venv'
-# EMAIL_HOST_PASSWORD = 'pass from venv'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
