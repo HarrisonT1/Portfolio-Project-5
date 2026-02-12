0a2dca993e0d01975cfdc991410c59f4f419c9a3
harrison townend <harrisontownend@hotmail.com>
2026-02-12 16:44:38 +0000

    Allowed caching of images for a long time

--- sugar_rush/settings.py
+++ sugar_rush/settings.py
@@ -224,6 +224,11 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
 # S3 BUCKET
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'project-5-sugar-rush'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
