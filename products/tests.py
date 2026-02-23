6ed44d45bd524e3ec4bf589089851d5250af336f
harrison townend <harrisontownend@hotmail.com>
2026-02-23 08:29:33 +0000

    Added test in products/tests.py for automatically generated slug

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from decimal import Decimal
+
+
+from .models import Product
+
 
 # Create your tests here.
+
+
+class ProductModelTest(TestCase):
+    def test_automatic_generated_slug(self):
+        product = Product.objects.create(
+            name="Test Product",
+            price=Decimal("3.00"),
+            description="Add Test Product",
+            stock_level=50
+        )
+
+        self.assertEqual(product.slug, "test-product")
