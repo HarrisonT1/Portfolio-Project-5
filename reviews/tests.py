183a5bbf3690008a73d50bc96d94feb9d5c02982
harrison townend <harrisontownend@hotmail.com>
2026-02-23 12:05:35 +0000

    Added a test for creating a review

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Review
+from accounts.models import UserAccount
 
 # Create your tests here.
+
+
+class ReviewModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test",
+            email="test@test.com",
+            password="123",
+        )
+        self.profile = UserAccount.objects.create(user=self.user)
+
+    def test_review_creation(self):
+        review = Review.objects.create(
+            user=self.user,
+            rating=5,
+            review_message="test message",
+        )
+        self.assertEqual(
+            str(review), f"{self.user.username} - {review.review_message}")
