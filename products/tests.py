6eb6577f884f122eceea7d4c085080a7ae38f334
harrison townend <harrisontownend@hotmail.com>
2026-02-23 08:46:15 +0000

    Added test in product/test.py for creating the order total on checkout

--- products/tests.py
+++ products/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from decimal import Decimal
+from checkout.models import Order
+from checkout.utils import create_line_items
 
 
 from .models import Product
@@ -18,3 +20,43 @@ class ProductModelTest(TestCase):
         )
 
         self.assertEqual(product.slug, "test-product")
+
+
+class CreateLineItemsTest(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(
+            name="Test Product",
+            slug="test-product",
+            price=Decimal("3.00"),
+            description="Add Test Product",
+            stock_level=75,
+        )
+
+        self.order = Order.objects.create(
+            user=None,
+            full_name="test test",
+            email="test@test.com",
+            phone_number="123456789",
+            country="GB",
+            postcode="test",
+            city="test",
+            town="test",
+            street_address1="test",
+            delivery_method="standard",
+            stripe_pid="test",
+        )
+
+    def test_calculate_order_total(self):
+        bag = {
+            "test-product": 3
+        }
+
+        initial_stock = self.product.stock_level
+
+        create_line_items(bag, order=self.order)
+
+        self.product.refresh_from_db()
+
+        self.assertEqual(
+            self.product.stock_level, initial_stock - 3
+        )
