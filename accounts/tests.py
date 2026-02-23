ad5eaae9d8f9415d4e1690f0071d92fc089130af
harrison townend <harrisontownend@hotmail.com>
2026-02-23 11:33:26 +0000

    Added a test for account creation in accounts/tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserAccount
 
 # Create your tests here.
+
+
+class UserAccountModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test",
+            email="test@test.com",
+            password="123",
+        )
+
+    def test_user_account_create(self):
+        profile = UserAccount.objects.create(user=self.user)
+        self.assertEqual(profile.user.username, self.user.username)
