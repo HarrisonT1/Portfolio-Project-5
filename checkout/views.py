42c9ed2fa37f7578ccc0a771fdcaa7c2e77771ae
harrison townend <harrisontownend@hotmail.com>
2026-03-02 19:48:53 +0000

    Fix metadata inside paymentintent modify

--- checkout/views.py
+++ checkout/views.py
@@ -196,13 +196,16 @@ def cache_checkout_data(request):
     Saves delivery method name and save-info
     """
     try:
-        email = request.POST.get('id_email')
+        email = request.POST.get('email')
         print(email)
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
             'save_info': request.POST.get('save_info'),
-            'username': request.user.username,
+            'username': (
+                request.user.username
+                if request.user.is_authenicated
+                else 'Guest'),
             'email': email,
         })
         return HttpResponse(status=200)
